e99459e50e163e07e42c08568b3af80217ccef21
Vraiment <jemc44@gmail.com>
2018-05-23 15:27:27 -0700

    Added Native file

---
+++ src/native.kt
@@ -0,0 +1,11 @@
+@file:JvmName("Native")
+
+external fun sayHello()
+
+external fun sendANum(arg: Int)
+
+external fun sendAString(arg: String)
+
+external fun getANum(): Int
+
+external fun getAString(): String
